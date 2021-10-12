320c674f382cbbc923843af9bebcc1e443750b52
ysoroko <ysoroko@student.s19.be>
2021-10-12 12:19:15 +0200

    ex00 Bureaucrat class fully implemented, Exceptions still to add to .cpp

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 10:56:49 by ysoroko           #+#    #+#             */
+/*   Updated: 2021/10/12 12:18:10 by ysoroko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+
+// --------------------------- Bureaucrat Functions ----------------------------
+
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(150)
+{
+	std::cout << "A default Bureaucrat has beed constructed\n";
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name)
+{
+	if (grade > 150)
+		throw (BureaucratExceptionGradeTooHigh());
+	else if (grade < 1)
+		throw (BureaucratExceptionGradeTooLow());
+	else
+	{
+		this->_grade = grade;
+		std::cout << "A Bureaucrat " << _name << " with a grade " << _grade << " has beed constructed\n";
+	}
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "A Bureaucrat " << _name << " with a grade " << _grade << " has beed destroyed\n";
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const & ref) : _name(ref.getName() + "_copy")
+{
+	this->_grade = ref.getGrade();
+	std::cout << "A Bureaucrat " << _name << " with a grade " << _grade << " has beed constructed from a copy\n";
+}
+
+Bureaucrat & Bureaucrat::operator=(Bureaucrat const & ref)
+{
+	this->_grade = ref.getGrade();
+	return (*this);
+}
+
+void				Bureaucrat::incrementGrade( Bureaucrat & ref )
+{
+	if (ref.getGrade() - 1 < 1)
+		throw (BureaucratExceptionGradeTooLow());
+	else
+	{
+		this->_grade--;
+	}
+}
+
+void				Bureaucrat::decrementGrade( Bureaucrat & ref )
+{
+	if (ref.getGrade() + 1 > 150)
+		throw (BureaucratExceptionGradeTooHigh());
+	else
+	{
+		this->_grade++;
+	}
+}
+
+
+// Getters
+const std::string	Bureaucrat::getName( void ) const
+{
+	return (this->_name);
+}
+
+const int			Bureaucrat::getGrade( void ) const
+{
+	return (this->_grade);
+}
+
+// Setters
+void				Bureaucrat::setGrade( const int new_grade)
+{
+	 this->_grade = new_grade;
+}
+
+// << operator overload
+std::ostream & operator<<( std::ostream & o, Bureaucrat const & rhs)
+{
+	o << "Bureaucrat " << rhs.getName() << " has a grade " << rhs.getGrade() << std::endl;
+	return (o);
+}
+
+// ----------------------------------------------------------------------------
+
+
+
+// ---------------------- BureaucratException Functions -----------------------
---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 10:56:53 by ysoroko           #+#    #+#             */
+/*   Updated: 2021/10/12 12:07:15 by ysoroko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class BureaucratException : public std::exception
+{
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("BureaucratException");
+			//in "throw" we can tell which exception it can throw
+		}
+};
+
+class BureaucratExceptionGradeTooLow : public BureaucratException
+{
+	public:
+		virtual const char* what() const throw() //in "throw" we can tell which exception it can throw
+		{
+			return ("Cannot get a grade < 1!");
+		} 
+};
+
+class BureaucratExceptionGradeTooHigh : public BureaucratException
+{
+	public:
+		virtual const char* what() const throw() //in "throw" we can tell which exception it can throw
+		{
+			return ("Cannot get a grade > 150!");
+		}
+};
+
+class Bureaucrat
+{
+	public:
+		// Constructors / destructor / '=' overload
+		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
+		~Bureaucrat();
+		Bureaucrat(Bureaucrat const & ref);
+		Bureaucrat & operator=(Bureaucrat const & ref);
+
+		// Increment / Decrement grade
+		void				incrementGrade( Bureaucrat & ref );
+		void				decrementGrade( Bureaucrat & ref );
+
+		// Getters
+		const std::string	getName( void ) const;
+		const int			getGrade( void ) const;
+
+		// Setters
+		void				setGrade( const int new_grade);
+
+	private:
+		const std::string	_name;
+		int					_grade; //1 = highest, 150 = lowest
+
+};
+
+// << overload
+std::ostream & operator<<( std::ostream & o, Bureaucrat const & rhs);
+
+
+#endif
